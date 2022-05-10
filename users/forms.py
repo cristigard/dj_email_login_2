35905e619d18de658d061e7d9594bef34ee2567f
Cristi Gard <65028018+cristigard@users.noreply.github.com>
2022-05-10 18:18:04 +0300

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -10,8 +10,24 @@ from django.contrib.auth.forms import (UserCreationForm, AuthenticationForm,
 class RegisterForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username', 'password1', 'password2')
-
+        fields = ['email', 'username','password1', 'password2']
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px; ',
+                }),
+            'username': forms.TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                }),
+            }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'style': 'max-width: 300px;'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'style': 'max-width: 300px;'})
+        
+        
 
 class CutomUserUpdateForm(forms.ModelForm):
     class Meta:
